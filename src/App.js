9b619e1597bde21f006c92e8e2c54ceee1c86a74
Femi <ologunwafemi@gmail.com>
2021-05-26 14:59:30 +0100

    setup routing that determines components to be mounted depending on the url

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import About from './components/About';
-import Footer from './components/Footer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 //My components
 import Header from './components/Header';
 import HomeGuest from './components/HomeGuest';
+import About from './components/About';
 import Terms from './components/Terms';
+import Footer from './components/Footer';
 
 function App() {
    return (
-      <>
+      <BrowserRouter>
          <Header />
-         {/* <HomeGuest /> */}
-         {/* <About /> */}
-         <Terms />
+         <Switch>
+            <Route path='/' exact>
+               <HomeGuest />
+            </Route>
+            <Route path='/about-us'>
+               <About />
+            </Route>
+            <Route path='/terms'>
+               <Terms />
+            </Route>
+         </Switch>
          <Footer />
-      </>
+      </BrowserRouter>
    );
 }
 
